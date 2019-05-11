61b8ea0eca859882173ed61ce605b1d2c17cab73
bartvanhoey <bartvanhoey@hotmail.com>
2019-05-11 08:40:49 +0200

    end part 40

--- Repositories/MockEmployeeRepository.cs
+++ Repositories/MockEmployeeRepository.cs
@@ -8,9 +8,9 @@ namespace EmployeeManagement.Repositories
     {
         private readonly List<Employee> _employees = new List<Employee>()
         {
-            new Employee {Id = 1, Name = "Mary", Department = "HR", Email = "mary@pragimtech.com"},
-            new Employee {Id = 2, Name = "John", Department = "Accounting", Email = "john@pragimtech.com"},
-            new Employee {Id = 3, Name = "Mark", Department =  "IT", Email = "mark@pragimtech.com"}
+            new Employee {Id = 1, Name = "Mary", Department = Dept.HR, Email = "mary@pragimtech.com"},
+            new Employee {Id = 2, Name = "John", Department = Dept.IT, Email = "john@pragimtech.com"},
+            new Employee {Id = 3, Name = "Mark", Department =  Dept.IT, Email = "mark@pragimtech.com"}
         };
         
         public Employee GetEmployee(int id) => _employees.FirstOrDefault(x => x.Id == id);
