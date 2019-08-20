ca07f240120f1e3a44aaaffd223e797a69a31b7f
bartvanhoey <bartvanhoey@hotmail.com>
2019-08-20 09:42:25 +0200

    end part 75

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -65,9 +65,22 @@ namespace EmployeeManagement.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+        [AcceptVerbs("Get", "Post")]
+        [AllowAnonymous]
+        public async Task<IActionResult> IsEmailInUse(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null) {
+                return Json(true);
+            }
+            else {
+                return Json($"Email {email} is already in use");
+            }
+        }
+
+
         [HttpPost]
         [AllowAnonymous]
-
         public async Task<IActionResult> Register(RegisterViewModel model)
         {
 
